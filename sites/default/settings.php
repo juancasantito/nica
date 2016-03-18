ee5fc61b50a645f3437cc1d40101d4a8f244ef1b
Juan <juancasantovenia@gmail.com>
2016-03-18 11:01:45 -0600

    setup patheon

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -706,8 +706,18 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 $settings['config_readonly'] = TRUE;
 $settings['install_profile'] = 'config_installer';
-$config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/config_kiAoJEvYbDug-MYtyMpQ9xyHgrasKqDohb1kTzQwjqz_n72h6RnfHNJCvitDLKILomWJNFsmYg/sync';
 
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to insure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . '/settings.pantheon.php';
