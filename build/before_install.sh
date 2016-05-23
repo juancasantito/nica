d66625fee6483ba3f8d817cb2c4366b72ea5ac5c
Edys Meza <edysmeza@gmail.com>
2016-05-23 16:50:08 -0600

    use composer instance of composer manager module

--- build/before_install.sh
+++ build/before_install.sh
@@ -5,4 +5,4 @@ phpenv config-rm xdebug.ini
 echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 composer self-update
 # Provide db connection string for installation.
-printf "<?php\n\$databases['default']['default'] = ['database' => 'travis', 'username' => 'root', 'password' => '', 'prefix' => '', 'host' => 'localhost', 'port' => '3306', 'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql', 'driver' => 'mysql'];" > ./sites/default/settings.local.php
+printf "<?php\n\$databases['default']['default'] = ['database' => 'travis', 'username' => 'root', 'password' => '', 'prefix' => '', 'host' => 'localhost', 'port' => '3306', 'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql', 'driver' => 'mysql'];" > ./web/sites/default/settings.local.php
--- build/install.sh
+++ build/install.sh
@@ -3,5 +3,6 @@ set -ev
 mysql -e 'create database IF NOT EXISTS travis;' -uroot
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 composer global require drush/drush:dev-master --dev --prefer-dist
+cd ${TRAVIS_BUILD_DIR}/web
 phpenv rehash
 drush site-install config_installer -y --verbose --db-url=mysql://root:@127.0.0.1/travis --db-su='root' --db-su-pw=''
