99b3767039cbea4242851cdc3f2f6c96b3db2496
Edys Meza <edysmeza@gmail.com>
2016-05-23 17:01:38 -0600

    use composer instance of composer manager module

--- build/before_install.sh
+++ build/before_install.sh
@@ -2,7 +2,16 @@
 set -ev
 phpenv config-rm xdebug.ini
 # Disable sendmail for local PHP for install.
-echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-composer self-update
-# Provide db connection string for installation.
-printf "<?php\n\$databases['default']['default'] = ['database' => 'travis', 'username' => 'root', 'password' => '', 'prefix' => '', 'host' => 'localhost', 'port' => '3306', 'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql', 'driver' => 'mysql'];" > ./web/sites/default/settings.local.php
+echo sendmail_path=`which true` >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/sendmail_disable.ini
+
+# Restore vendor directory from cache.
+if [[ ! -d ${HOME}/vendor-cache ]]; then mkdir ${HOME}/vendor-cache; fi
+echo "Vendor cache content:"
+ls -lh ~/vendor-cache/
+cd ${TRAVIS_BUILD_DIR}
+cachefile="`echo -n ${TRAVIS_PHP_VERSION} | sha1sum | cut -d " " -f 1`.tar"
+if [[ -f ~/vendor-cache/${cachefile} ]]; then tar -xf ~/vendor-cache/${cachefile}; fi
+if [[ -d ~/vendor/ ]]; then echo "Size of vendor directory extracted from cache:"; du -hs ~/vendor/; else echo "Vendor directory does not exist"; fi
+
+composer --verbose self-update
+composer --version
--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ev
-mysql -e 'create database IF NOT EXISTS travis;' -uroot
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-composer global require drush/drush:dev-master --dev --prefer-dist
+
+composer --verbose --prefer-source install
 cd ${TRAVIS_BUILD_DIR}/web
-phpenv rehash
-drush site-install config_installer -y --verbose --db-url=mysql://root:@127.0.0.1/travis --db-su='root' --db-su-pw=''
+printf "<?php\n\$databases['default']['default'] = ['database' => 'travis', 'username' => 'root', 'password' => '', 'prefix' => '', 'host' => 'localhost', 'port' => '3306', 'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql', 'driver' => 'mysql'];" > ${TRAVIS_BUILD_DIR}/web/sites/default/settings.local.php
+${TRAVIS_BUILD_DIR}/vendor/bin/drush site-install config_installer -y --verbose --db-url=mysql://root:@127.0.0.1/travis --db-su='root' --db-su-pw=''
+${TRAVIS_BUILD_DIR}/vendor/bin/drush runserver --dns localhost:8080 - &
+sleep 3
