249ea4cbe408ae623bbf5fc3f646e80464608d8a
Edys Meza <edysmeza@gmail.com>
2016-05-24 09:34:05 -0600

    use composer instance of composer manager module

--- build/travis/install.sh
+++ build/travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 
-composer --verbose --prefer-source install
+composer --verbose install
 cd ${TRAVIS_BUILD_DIR}/web
 printf "<?php\n\$databases['default']['default'] = ['database' => 'travis', 'username' => 'root', 'password' => '', 'prefix' => '', 'host' => 'localhost', 'port' => '3306', 'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql', 'driver' => 'mysql'];" > ${TRAVIS_BUILD_DIR}/web/sites/default/settings.local.php
 ${TRAVIS_BUILD_DIR}/vendor/bin/drush site-install config_installer -y --verbose --db-url=mysql://root:@127.0.0.1/travis --db-su='root' --db-su-pw=''
