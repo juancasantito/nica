cae989c342887d20886f47f6919eb60e6ca2676d
Luis Solórzano <luisnic20@gmail.com>
2016-06-23 17:19:21 -0600

    Crated Class: Created class ValidateDate

--- web/modules/custom/nica_migrate/src/ValidateDate.php
+++ web/modules/custom/nica_migrate/src/ValidateDate.php
@@ -9,17 +9,23 @@ namespace Drupal\nica_migrate;
 
 class ValidateDate {
 
-    public static function positionChangeDate($date_csv)
-    {
-      if (!empty($date_csv)) {
-        $data_value = explode('/', $date_csv);
-          return date("Y-m-d", mktime(0, 0, 0, $data_value[0], $data_value[1], $data_value[2]));
-        }else {
-            return "";
+  public static function validate($date_nica) {
+    $return = '';
+    if (!empty($date_nica)) {
+      $date_exploded = explode("-", $date_nica);
+      foreach ($date_exploded as $date) {
+        $date = explode('/', $date);
+        if (count($date) == 2) {
+          array_splice($date, 1, 0, [1]);
         }
+        if (count($date) == 1) {
+          array_splice($date, 0, 0, [1, 1]);
+        }
+        list($month, $day, $year) = $date;
+        $return[] = date('Y-m-d', mktime(0, 0, 0, $month, $day, $year));
+      }
     }
+    return $return;
+  }
 
-    public static function dateEmployment() {
-
-    }
 }
