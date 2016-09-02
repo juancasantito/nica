79491a2cb007e6f06a38a86fea5b68561ce5fa9b
Luis Solórzano <luisnic20@gmail.com>
2016-09-02 10:45:35 -0600

    Create Preprocess For Theme nica_theme

--- web/themes/custom/nica_theme/src/Plugin/Preprocess/NicaCustomCV.php
+++ web/themes/custom/nica_theme/src/Plugin/Preprocess/NicaCustomCV.php
@@ -2,38 +2,50 @@
 
 namespace Drupal\nica_theme\Plugin\Preprocess;
 
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\bootstrap\Plugin\Preprocess\PreprocessBase;
 use Drupal\bootstrap\Plugin\Preprocess\PreprocessInterface;
+use Drupal\bootstrap\Utility\Variables;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\datetime\Plugin\views\filter\Date;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Pre-processes variables for the "constancy" theme hook.
+ * Pre-processes variables for the "nica_custom_cv" theme hook.
  *
  * @ingroup plugins_preprocess
  *
- * @BootstrapPreprocess("constancy")
+ * @BootstrapPreprocess("nica_custom_cv")
  */
 
-class NicaConstancy extends PreprocessBase implements PreprocessInterface, ContainerInjectionInterface{
+class NicaCustomCV extends PreprocessBase implements PreprocessInterface, ContainerFactoryPluginInterface{
 
 	/** @var \Drupal\Core\Config\ConfigFactoryInterface $configFactory*/
 	protected $configFactory;
 
-	public function __construct(ConfigFactoryInterface $configFactory) {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct(array $configuration, $pluginId, $pluginDefinition, ConfigFactoryInterface $configFactory) {
+		parent::__construct($configuration, $pluginId, $pluginDefinition);
+
 		$this->configFactory = $configFactory;
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
-	public static function create(ContainerInterface $container) {
-		return new static($container->get('config.factory'));
+	public static function create(ContainerInterface $container, array $configuration, $pluginId, $pluginDefinition) {
+		return new static($configuration, $pluginId, $pluginDefinition,$container->get('config.factory'));
 	}
 
 	/**
 	 * {@inheritdoc}
 	 */
-	public function preprocess(array &$variables, $hook, array $info) {
+	public function preprocessVariables(Variables $variables) {
 		$nicaConfig = $this->configFactory->get('nica_custom.settings');
+		$variables->city = $nicaConfig->get('nica_city_project');
+		$variables->organization = $nicaConfig->get('nica_name_executor');
+		$variables->date = '2 de septiembre 2016';
 	}
 }
